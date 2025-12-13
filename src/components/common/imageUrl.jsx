27ae35fbf796f62f3cad69d14f84c2b24d548ccd
Mijanur Rahman <mijan.connect@gmail.com>
2025-12-13 16:49:40 +0600

    URL changed

--- src/components/common/imageUrl.jsx
+++ src/components/common/imageUrl.jsx
@@ -1,5 +1,6 @@
 export const getImageUrl = (path) => {
-  const baseUrl = import.meta?.env?.VITE_API_URL || "http://10.10.7.8:5004";
+  // const baseUrl = import.meta?.env?.VITE_API_URL || "http://10.10.7.8:5004";
+  const baseUrl = import.meta?.env?.VITE_API_URL || "https://44kb593x-5004.inc1.devtunnels.ms";
 
   if (!path || typeof path !== "string") {
     return "/images/default-avatar.png";
